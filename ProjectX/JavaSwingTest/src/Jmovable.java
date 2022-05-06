74ff52ac180a851f167426c05cffcb26245702c5
zoematr <zoe.matrullo@gmail.com>
2022-05-06 15:19:32 +0200

    zoe's branch

--- ProjectX/JavaSwingTest/src/Jmovable.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.*;
-
-public class Jmovable extends JLabel {
-    public int[] coor= {7854596,7845874};
-    public void Ucoor(int[] a){
-        coor= a;
-    }
-}
--- ProjectX/JavaSwingTest/src/printer.java
+++
@@ -1,9 +0,0 @@
-public class printer {
-    public static void main(String[] args) {
-        for (int i = 1; i <= 36; i++) {
-            System.out.print("timer.move(timer.start"+i+");");
-
-        }
-    }
-    Jmovable r= new Jmovable();
-}
